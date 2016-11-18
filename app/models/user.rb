74db44737322bd964974869bf75ed77e8c0335ec
Miles McArdle-Coe & Michael Bravo <pair+kndekaru.michaelbravo777@devbootcamp.com>
2016-11-18 10:56:14 -0500

    make user migration and model

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class user < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :username, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  has_secure_password
+
+  has_many :pitches
+  has_many :votes
+  belongs_to :cohorts
+
+end
