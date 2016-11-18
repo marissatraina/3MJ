c1a0de9a49c85bd9165b95f2f3246eb8bd0e71de
Miles McArdle-Coe & Michael Bravo <pair+kndekaru.michaelbravo777@devbootcamp.com>
2016-11-18 15:59:15 -0500

    add cohort admin links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,14 @@
 class SessionsController < ApplicationController
 
+  def index
+
+    if logged_in?
+      redirect_to '/cohorts'
+    else
+      render "new"
+    end
+  end
+
   def new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :r1votes
   resources :r2votes
   resources :cohorts
-  root 'sessions#new'
+  root 'sessions#index'
 end
